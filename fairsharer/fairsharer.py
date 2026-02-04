78328fd60b07e5a971301962997ab473a7e335e5
Vipusiny Vijayakumar <vipusiny.vijayakumar@study.hs-duesseldorf.de>
2026-02-04 16:04:34 +0100

    function def fair_sharer

---
+++ fairsharer/fairsharer.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def fair_sharer(values, num_iterations, share=0.1):
+    values = np.array(values, dtype=float)
+
+    for _ in range(num_iterations):
+        new_values = values.copy()
+
+        #the highest value and its index are being searched
+        max_index = np.argmax(values)
+        max_value = values[max_index]
+
+        #amount that will be shared 
+        share_amount = max_value * share
+
+        #neighbors of left and right
+        left = (max_index -1) % len(values)
+        right = (max_index + 1) % len(values)
+
+        #update values
+        new_values[max_index] -= 2 * share_amount 
+        new_values[left] += share_amount
+        new_values[right] += share_amount
+
+        #new values for tge next time
+        values = new_values
+    return new_values
