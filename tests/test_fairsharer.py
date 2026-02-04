fdf3b0c00a419753642f75f864b916da5346b6ee
Vipusiny Vijayakumar <vipusiny.vijayakumar@study.hs-duesseldorf.de>
2026-02-04 18:15:39 +0100

    Ignore .DS_Store files

---
+++ tests/test_fairsharer.py
@@ -0,0 +1,9 @@
+import numpy as np 
+from fairsharer.fairsharer import fair_sharer
+
+def test_fair_sharer():
+    result_1 = fair_sharer([0, 1000, 800, 0], 1)
+    assert result_1 == [100.0, 800.0, 900.0, 0.0]
+
+    result_2 = fair_sharer([0, 1000, 800, 0], 2)
+    assert result_2 == [100.0, 890.0, 720.0, 90.0]
